f814167f1a3946c8e6f3db1c68f2a4a0a406fa46
varunchoubey2000 <varun0choubey@gmail.com>
2025-01-15 13:44:33 +0530

    playing with the Routes

--- src/app.js
+++ src/app.js
@@ -3,22 +3,12 @@ const app = express();
 
 
 
-app.get("/user", (req, res) => {
+app.get("/user/:userID/:name/:password", (req, res) => {
+    console.log(req.params)
     res.send({ name: "Varun", lastName: "Choubey" })
 })
 
-app.post("/user", (req, res) => {
-    console.log("Save data to the database")
-    res.send("Data successfully saved to the database")
-})
 
-app.delete("/user", (req, res) => {
-    res.send("deleted successFully!")
-})
-
-app.use("/test", (req, res) => {
-    res.send("Hello from the server")
-})
 
 
 app.listen(777, () => {
