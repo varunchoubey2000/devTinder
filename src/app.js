311063a80d42846cbfa5f69d5a3c3b0962cd3008
varunchoubey2000 <varun0choubey@gmail.com>
2025-01-15 12:51:37 +0530

    exploring get Post delete

--- src/app.js
+++ src/app.js
@@ -1,16 +1,26 @@
 const express = require("express");
 const app = express();
 
+
+
+app.get("/user", (req, res) => {
+    res.send({ name: "Varun", lastName: "Choubey" })
+})
+
+app.post("/user", (req, res) => {
+    console.log("Save data to the database")
+    res.send("Data successfully saved to the database")
+})
+
+app.delete("/user", (req, res) => {
+    res.send("deleted successFully!")
+})
+
 app.use("/test", (req, res) => {
     res.send("Hello from the server")
 })
-app.use("/hello", (req, res) => {
-    res.send("Hello hello hello")
-})
-app.use("/", (req, res) => {
-    res.send("Hello from the Dashbourd")
-})
 
-app.listen(3000, () => {
-    console.log("Server is successfully listening on port 3000 ...")
+
+app.listen(777, () => {
+    console.log("Server is successfully listening on port 777 ...")
 });
