31791cd7312e9e9d746ba5ce801afb49bcd395c4
varunchoubey2000 <varun0choubey@gmail.com>
2025-01-15 14:27:13 +0530

    multiple_Route_handling

--- src/app.js
+++ src/app.js
@@ -2,15 +2,30 @@ const express = require("express");
 const app = express();
 
 
+app.use("/user", (req, res, next) => {
+    console.log("Handling the routes user")
+    // res.send("Response!!")
+    next();
+},
+    (req, res, next) => {
+        // console.log("Handling the routes user 2")
+        // res.send("2nd Response here..!!")
+        next();
+    },
+    (req, res, next) => {
+        // console.log("Handling the routes user 3")
+        // res.send("3nd Response here..!!")
+        next()
+    },
+    (req, res) => {
+        console.log("Handling the routes user 4")
+        res.send("4nd Response here..!!")
+    },
+),
 
-app.get("/user/:userID/:name/:password", (req, res) => {
-    console.log(req.params)
-    res.send({ name: "Varun", lastName: "Choubey" })
-})
 
 
 
-
-app.listen(777, () => {
-    console.log("Server is successfully listening on port 777 ...")
-});
+    app.listen(777, () => {
+        console.log("Server is successfully listening on port 777 ...")
+    });
