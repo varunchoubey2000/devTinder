457de780e88c7191fa8227948a814efe9b4f0c42
varunchoubey2000 <varun0choubey@gmail.com>
2025-01-16 14:38:46 +0530

    updating apis

--- src/app.js
+++ src/app.js
@@ -45,6 +45,30 @@ app.get("/feed", async (req, res) => {
         res.status(400).send("something went wrong")
     }
 })
+app.delete("/user", async (req, res) => {
+    const userId = req.body.userId;
+    try {
+        const user = await User.findByIdAndDelete(userId)
+        res.send("user Deleted successfully!! ")
+    } catch (error) {
+        res.status(400).send("something went wrong")
+    }
+})
+
+app.patch("/user", async (req, res) => {
+    const userId = req.body.userId;
+    const data = req.body;
+
+    try {
+        const user = await User.findByIdAndUpdate({ _id: userId }, data, {
+            returnDocument: "after",
+        })
+        console.log(user)
+        res.send("User Updated Sucessfully");
+    } catch (error) {
+        res.status(400).send("something went wrong")
+    }
+})
 
 connectDB().then(() => {
     console.log("Database connection established");
