747a06e527e985385a169f9af72c2526e25a835f
varunchoubey2000 <varun0choubey@gmail.com>
2025-01-16 13:56:39 +0530

    get-Apis

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/database")
-const User = require("./models/user")
+const User = require("./models/user");
+const user = require("./models/user");
 const app = express();
 
 app.use(express.json())
@@ -12,9 +13,37 @@ app.post("/signup", async (req, res) => {
         await user.save();
         res.send("user added successfully")
     } catch (err) {
-        res.status(400).send("Error saving the user"+ err.message)
+        res.status(400).send("Error saving the user" + err.message)
     }
 
+})
+
+app.get("/user", async (req, res) => {
+    const userEmail = req.body.emailId;
+
+
+    try {
+        const users = await User.findOne({ emailId: userEmail })
+        res.send(users)
+        //     const users = await User.find({ emailId: userEmail })
+        //     if (users.length === 0) {
+        //         res.status(400).send("User not found")
+        //     }
+        //     else {
+        //         res.send(users)
+
+        //     }
+    } catch (error) {
+        res.status(400).send("something went wrong")
+    }
+})
+app.get("/feed", async (req, res) => {
+    try {
+        const users = await User.find({})
+        res.send(users)
+    } catch (error) {
+        res.status(400).send("something went wrong")
+    }
 })
 
 connectDB().then(() => {
